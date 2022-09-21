e7879673365128243395824d5beb97fdd052baa6
Karthik <msdhamodharan5@gmail.com>
2022-09-22 00:26:11 +0530

    implemented pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+// Whenever nest.js encounters any of decortors , it will know to execute
+app.useGlobalPipes(new ValidationPipe()); // Mandatory to enable pipes
+
+await app.listen(3000);
 }
 bootstrap();
