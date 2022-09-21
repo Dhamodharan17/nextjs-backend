8ce8333eeb8251d1dea530b3ee266b3c20ca8db2
Karthik <msdhamodharan5@gmail.com>
2022-09-21 18:19:01 +0530

    implemented dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,29 +1,37 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { title } from 'process';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
- constructor(private tasksService: TasksService){}
+  constructor(private tasksService: TasksService) {}
 
- @Get()
- getAllTasks():Task[]{  // Adding for type safety - return type as Task[]
+  @Get()
+  getAllTasks(): Task[] {
+    // Adding for type safety - return type as Task[]
     return this.tasksService.getAllTasks();
- }
-/* METHOD 1 - Gets all paramters
+  }
+  /* METHOD 1 - Gets all paramters
  @Post()
 createTask(@Body() body){
 console.log('body',body)
 }
 */
 
-// METHOD 2
+  // METHOD 2
+ /* @Post()
+  createTask(@Body('title') title, @Body('description') description): Task {
+    return this.tasksService.createTask(title, description);
+  }
+*/
+
+
 @Post()
-createTask(@Body('title') title, @Body('description') description):Task{
-
-    return this.tasksService.createTask(title,description);
-}
+createTask(@Body() createTaskDto : CreateTaskDto):Task{
+return this.tasksService.createTask(createTaskDto);
 }
 
 
+}
