5bcb08537fb529d6f42039300f1782287a386e3e
Karthik <msdhamodharan5@gmail.com>
2022-09-21 18:12:39 +0530

    Implemeted Model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,9 +8,22 @@ export class TasksController {
  constructor(private tasksService: TasksService){}
 
  @Get()
- getAllTasks(){
+ getAllTasks():Task[]{  // Adding for type safety - return type as Task[]
     return this.tasksService.getAllTasks();
  }
-
-
+/* METHOD 1 - Gets all paramters
+ @Post()
+createTask(@Body() body){
+console.log('body',body)
 }
+*/
+
+// METHOD 2
+@Post()
+createTask(@Body('title') title, @Body('description') description):Task{
+
+    return this.tasksService.createTask(title,description);
+}
+}
+
+
