237879b9cf4d6d9b91ba5711669481e639403b26
Karthik <msdhamodharan5@gmail.com>
2022-09-24 15:41:15 +0530

    guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([UsersRepository])],
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports:[
+    PassportModule.register({defaultStrategy:'jwt'}),
+    JwtModule.register({
+      secret : 'topSecret51',
+      signOptions : {
+        expiresIn: 3600
+      }
+    }),
+    TypeOrmModule.forFeature([UsersRepository])],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports:[JwtStrategy,PassportModule] // by this we are allowing any other module which imports auth module can also enjoy this 2 modules
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { throws } from 'assert';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
@@ -10,9 +13,25 @@ export class AuthService {
     constructor(
         @InjectRepository(UsersRepository)
         private usersRespository :UsersRepository,
+        private jwtService : JwtService
     ){}
 
 async signUp(authCredentialsDto: AuthCredentialsDto) : Promise<void>{
     return this.usersRespository.createUser(authCredentialsDto);
+}
+
+
+async signIn(authCredentialsDto:AuthCredentialsDto) : Promise<{accessToken : string}>{
+    const {username, password} = authCredentialsDto;
+    const user = await this.usersRespository.findOne({username});
+
+    if(user && (await bcrypt.compare(password, user.password))){
+
+        const payload: JwtPayload = {username};
+        const accessToken = await this.jwtService.sign(payload);
+        return {accessToken}
+    }else{
+        throw  new UnauthorizedException('Please check your login credentials');
+    }
 }
 }
